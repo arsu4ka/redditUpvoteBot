9221f75b6ad76ed07bea861e738ef8804f8c71a5
arsu4ka <101658533+arsu4ka@users.noreply.github.com>
2022-08-11 23:31:15 +0300

    Update reddit_action.py

--- reddit/reddit_action.py
+++ reddit/reddit_action.py
@@ -18,7 +18,7 @@ class RedditProfile():
     
     def login(self) -> None:
         logger.info(f"Logging into reddit account '{self.username}'")
-        formResponse = self.session.get("https://www.reddit.com/login/")
+        formResponse = self.session.get("https://www.reddit.com/login/", proxies=self.proxy)
         soup = BeautifulSoup(formResponse.text, "lxml")
 
         csrf_token = soup.find("input", {'name': 'csrf_token'})["value"]
@@ -29,7 +29,7 @@ class RedditProfile():
             'dest': 'https://www.reddit.com/',
             'username': self.username,
         }
-        r = self.session.post('https://www.reddit.com/login', data=data)
+        r = self.session.post('https://www.reddit.com/login', data=data, proxies=self.proxy)
 
         if int(r.status_code) != 200:
             logger.error("Unexpected error occured while logging in")
@@ -37,7 +37,7 @@ class RedditProfile():
 
         self.logged = True
 
-        r = self.session.get("https://www.reddit.com/")
+        r = self.session.get("https://www.reddit.com/", proxies=self.proxy)
 
         soup = BeautifulSoup(r.text, "lxml")
         script_block = soup.find("script", {"id": "data"}).text
@@ -72,7 +72,7 @@ class RedditProfile():
             'authorization': f'Bearer {self.token}'
         }
 
-        response = self.session.post('https://oauth.reddit.com/api/vote', params=params, data=data, headers=headers)
+        response = self.session.post('https://oauth.reddit.com/api/vote', params=params, data=data, headers=headers, proxies=self.proxy)
 
         if int(response.status_code) != 200:
             logger.error("Unexpected error occured while upvoting the post")
