637823af9b7e6f81428b4b751a3aac4a1ae745ca
arsu4ka <101658533+arsu4ka@users.noreply.github.com>
2022-09-05 09:26:31 +0300

    Update connection.py

--- reddit/connection.py
+++ reddit/connection.py
@@ -1,5 +1,6 @@
-from reddit.reddit_action import RedditProfile
+from reddit import syncProfile
 from loguru import logger
+import time, random
 
 
 def read_txt(path: str) -> list:
@@ -13,41 +14,57 @@ def read_txt(path: str) -> list:
                 "username": cred_list[0],
                 "password": cred_list[1],
                 "host": cred_list[2],
-                "port": cred_list[3]
+                "port": cred_list[3],
+                "proxy_user": cred_list[4],
+                "proxy_pass": cred_list[5]
             })
     return res
 
 
-def run(fromFile: bool, data: str, reddit_post: str):
+def sync_run(credentials, reddit_post, upvote, sleep_range):
+    for acc in credentials:
+        try:
+            profile = syncProfile.RedditProfile(
+                acc["username"],
+                acc["password"],
+                {"host": acc["host"], "port": acc["port"], "proxy_user": acc["proxy_user"], "proxy_pass": acc["proxy_pass"]}
+            )
+            profile.login()
+            time.sleep(random.randrange(sleep_range[0], sleep_range[1]))
+            profile.vote(reddit_post, upvote)
+        except:
+            continue
+
+
+def async_run():
+    pass
+    
+
+def run(fromFile: bool, data: str, reddit_post: str, upvote: bool, sleep_range: tuple, sync: bool = True):
     if fromFile:
         try:
             cred = read_txt(data)
         except:
-            raise "Invalid file path!"
+            logger.error("Invalid file path!")
+            return
     else:
         try:
-            tempList = data.split(":")
+            tempList = data.strip().split(":")
+            for i in range(len(tempList)):
+                tempList[i] = tempList[i].strip()
             cred = [{
                 "username": tempList[0],
                 "password": tempList[1],
                 "host": tempList[2],
-                "port": tempList[3]  
+                "port": tempList[3] ,
+                "proxy_user": tempList[4],
+                "proxy_pass": tempList[5]
             }]
         except:
-            raise "Invalid credentials input!"
+            logger.error("Invalid credentials input!")
+            return
     
-    for acc in cred:
-        try:
-            profile = RedditProfile(
-                username=acc["username"],
-                password=acc["password"],
-                proxy={"host": acc["host"], "port": acc["port"]}
-            )
-            profile.login()
-            profile.upvote(reddit_post=reddit_post)
-        except:
-            continue
-    
-    logger.info("Actions completed! Check previous output to understand if everything went successfully.")
+    sync_run(cred, reddit_post, upvote, sleep_range) if sync else async_run()
 
+    logger.info("Actions completed! Check previous output to understand if everything went successfully.")
     
